4779355b70aebe473b1f22ef47b2dda792a4ea77
SenaProfeAlbeiro <earamos42@misena.edu.co>
2021-08-06 07:17:41 -0500

    Vista Empresa Sistema de Información

--- app/2_app_vistas_proyecto/js/scripts.js
+++ app/2_app_vistas_proyecto/js/scripts.js
@@ -5,17 +5,6 @@ const cuandoSeHaceClick = function (evento) {
 		document.getElementById("navbarSupportedContent").classList.toggle('show');		
 	}
 }
-
-// const color_fondo = function (evento) {
-// 	evento.target.style.background = "#6C757D";
-// 	evento.target.style.color = "#fff";
-// }
-
-// const color_fondo2 = function (evento) {
-// 	evento.target.style.background = "";
-// 	evento.target.style.color = "#007bff";
-// }
-
 // botones es un arreglo así que lo recorremos
 botones.forEach(boton => {
 	//Agregar listener
@@ -35,6 +24,29 @@ form.addEventListener('submit', function (event) {
 		alert("Datos Incorrectos");
 		document.getElementById('contrasena').value = "";
 		document.getElementById('usuario').value = "";		
+		event.preventDefault();		
+	}		
+});
+
+// Registro
+form2  = document.getElementById('registro');
+form2.addEventListener('submit', function (event) {
+	nombres  = document.getElementById('nombres').value;
+	apellidos  = document.getElementById('apellidos').value;
+	correo  = document.getElementById('correo').value;
+	contrasena  = document.getElementById('contrasena_reg').value;
+	confirmacion  = document.getElementById('confirmacion').value;
+	
+	if (nombres != "" && apellidos != "" && correo != "" && contrasena != "" && confirmacion != "") {
+		if (contrasena == confirmacion) {
+			alert("Los datos han sido enviado satisfactoriamente, por favor espere el correo de confirmación por parte del administrador");
+		} else {
+			alert("La contraseña no coincide con la confirmación");
+			document.getElementById('confirmacion').value = "";
+			event.preventDefault();		
+		}
+	} else {
+		alert("Diligencie todos los campos con los datos correctos");
 		event.preventDefault();		
 	}		
 });
