daf486f5f95286d319508bf6479469afe6822dce
SenaProfeAlbeiro <earamos42@misena.edu.co>
2021-08-11 13:49:22 -0500

    Programación Estructurada en PHP

--- app/3_app_pe-poo-mvc-con/2_programacion_php/1_pe/12_repetición_do_while_2/index.php
+++ app/3_app_pe-poo-mvc-con/2_programacion_php/1_pe/12_repetición_do_while_2/index.php
@@ -1,3 +1,11 @@
+<?php 	
+
+// declaración e inicialización de variables y constantes
+	$nom_aplicacion = "Aplicación";
+	$instruc = "Seleccione una opción del menú";
+
+// Salida
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,13 +14,20 @@
 	<title><?php echo $nom_aplicacion ?></title>
 </head>
 <body>
-	<h1>Aplicación</h1>	
+	<h1><?php echo $nom_aplicacion ?></h1>
+	<p><?php echo $instruc ?></p>
 	<hr>	
 	<ol>
 		<li><a href="1_calculadora.php">Calculadora</a></li>
 		<li><a href="2_factorial.php">Factorial</a></li>
 		<li><a href="3_fibonacci.php">Fibonacci</a></li>		
 		<li><a href="4_serie_par_impar.php">Serie par o impar</a></li>		
+		<li><a href="5_burbuja.php">Burbuja</a></li>		
+		<li><a href="6_vector_ordenado_estatico.php">Vector Ordenado (Estático)</a></li>		
+		<li><a href="7_vector_ordenado_dinamico.php">Vector Ordenado (Dinámico)</a></li>		
+		<li><a href="8_vector_guayabita.php">Guayabita</a></li>
+		<li><a href="9_matriz_datos_personales.php">Datos personales (Matriz)</a></li>		
+		<li><a href="10_matriz_tiendita.php">Tiendita el Chavo</a></li>		
 	</ol>	
 </body>
 </html>
