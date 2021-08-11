e01c88bcd151b0c2d33d634a5bea650d20e6bbfe
SenaProfeAlbeiro <earamos42@misena.edu.co>
2021-08-11 13:30:45 -0500

    Programación Estructurada en PHP

---
+++ app/3_app_pe-poo-mvc-con/2_programacion_php/1_pe/12_repetición_do_while_2/2_factorial.php
@@ -0,0 +1,56 @@
+<?php 
+	
+// declaración e inicialización de variables y constantes
+	$nom_aplicacion = "Calculadora Avanzada";
+	$instruc = "Encienda la calculadora";
+	$encender = "disabled";
+	$num1 = 0;
+	$aux = 0;
+	$res = "";
+	$factorial = 1;
+	$band = false;
+
+// entrada
+	if (isset($_GET['band'])) {
+		$band = boolval($_GET["band"]);
+		if ($band) {
+			$encender = "";
+			$instruc = "Digite el factorial / Enviar";			
+		} 
+	} 
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		$num1 = $_POST['numero'];		
+	}
+
+// Proceso
+	while ($num1 > 1) {
+		$factorial = $factorial * $num1;
+		$num1 = $num1 - 1;
+	}
+	$res = "El Factorial del número " . $aux . " es " . $factorial;
+
+// salida
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?php echo $nom_aplicacion ?></title>
+</head>
+<body>
+	<h1><?php echo $nom_aplicacion; ?></h1>
+	<h3><?php echo $instruc ?></h3>
+	<ul>
+		<li><a href="?band=1">Encender calculadora avanzada</a></li>
+		<li><a href="?band=">Apagar calculadora avanzada</a></li>
+		<li><a href="index.php">Volver</a></li>
+	</ul>
+	<form action="" method="POST">		
+		<label for="numero">Factorial</label>
+		<input type="text" name="numero" id="numero" <?php echo $encender ?>>
+		<input type="submit" name="submit" value="Enviar" <?php echo $encender ?>>
+	</form>
+	<h1><?php echo $res ?></h1>
+</body>
+</html>
---
+++ app/3_app_pe-poo-mvc-con/2_programacion_php/1_pe/12_repetición_do_while_2/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?php echo $nom_aplicacion ?></title>
+</head>
+<body>
+	<h1>Aplicación</h1>	
+	<hr>	
+	<ol>
+		<li><a href="1_calculadora.php">Calculadora</a></li>
+		<li><a href="2_factorial.php">Factorial</a></li>
+		<li><a href="3_fibonacci.php">Fibonacci</a></li>		
+		<li><a href="4_serie_par_impar.php">Serie par o impar</a></li>		
+	</ol>	
+</body>
+</html>
