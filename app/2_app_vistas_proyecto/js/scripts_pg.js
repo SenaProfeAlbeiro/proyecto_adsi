eca5f0948f7e9d4f3cd3af6fad3a6188be0fcba2
SenaProfeAlbeiro <earamos42@misena.edu.co>
2021-08-12 04:54:11 -0500

    Terminando Vistas Sistema de Información

--- app/2_app_vistas_proyecto/js/scripts_pg.js
+++ app/2_app_vistas_proyecto/js/scripts_pg.js
@@ -1,21 +1,29 @@
-// Elementos del DOM
+// Declaración e inicialización de variables
 var tabla = document.getElementById("tbl_consultar");
+
+// Crear controles
+var inputNombres = "<input type='text' class='nombres'/>";
 
 // Actualizar
 const btn_actualizar = document.querySelectorAll(".actualizar");
 const actualizar = function (event) {	
 	let i = this.parentNode.parentNode.rowIndex;
-	alert(i);
+	let celdas = tabla.rows[i];
+
+	let nombres = celdas.cells[1].firstChild.data;
+
+	celdas.cells[1].innerHTML = inputNombres;
+	
+	document.querySelector('input.nombres').value = nombres;
+	
+	event.preventDefault();
+
+
+
+
+	// alert("HolaMundo");
 	
 }
 btn_actualizar.forEach(boton => {
 	boton.addEventListener("click", actualizar);	
 });
-
-// Eliminar
-/*
-btn_eliminar  = document.getElementById('eliminar');
-btn_eliminar.addEventListener('click', function (event) {
-	alert("Voy a eliminar");
-});
-*/
