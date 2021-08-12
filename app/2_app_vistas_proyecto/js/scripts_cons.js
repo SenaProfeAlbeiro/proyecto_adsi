7564d2750a12bdaf3c3ee3425cad7496e82a8360
SenaProfeAlbeiro <earamos42@misena.edu.co>
2021-08-12 12:30:45 -0500

    Terminando Vistas Sistema de Información

--- app/2_app_vistas_proyecto/js/scripts_cons.js
+++ app/2_app_vistas_proyecto/js/scripts_cons.js
@@ -13,9 +13,20 @@ var btnEliminar = '<a class="btn btn-danger btn-sm mx-1" onclick="eliminar(this)
 
 // insertar
 function insertar(){
-	let nombres = localStorage.getItem('nombres');
+	// Recuperar los datos de variables de sesion
+	let doc_identidad = sessionStorage.getItem('doc_identidad');
+	let correo = sessionStorage.getItem('correo');
+	let nombres = sessionStorage.getItem('nombres');
+	let apellidos = sessionStorage.getItem('apellidos');
+	let contrasena_us = sessionStorage.getItem('contrasena_us');
+	let confirmacion = sessionStorage.getItem('confirmacion');
+	let perfil = sessionStorage.getItem('perfil');
+	let estado = sessionStorage.getItem('estado');
+	// Cantidad de filas
 	let i = tabla.rows.length;
+	// Inserta fila
 	let fila = tabla.insertRow(i);
+	// Inserta celdas
 	let celda = [];	
 	// Colocar cantidad de celdas
 	for (var cont = 0; cont < 8; cont++) {
@@ -23,7 +34,13 @@ function insertar(){
 	}
 	// Insertar datos a las celdas
 	celda[0].innerHTML = i;	
-	celda[3].innerHTML = nombres;
+	celda[1].innerHTML = doc_identidad;	
+	celda[2].innerHTML = correo;	
+	celda[3].innerHTML = nombres;	
+	celda[4].innerHTML = apellidos;	
+	celda[5].innerHTML = contrasena_us;	
+	celda[6].innerHTML = perfil;
+	celda[7].innerHTML = estado;
 	celda = fila.appendChild(cln);	
 }
 
@@ -36,9 +53,9 @@ function actualizar(fila){
 	prueba = "Actualizando";
 	celdas = tabla.rows[i];
 	// Obtiene el valor de la celda según su posición en la fila
-	nombres = celdas.cells[1].firstChild.data;	
+	nombres = celdas.cells[3].firstChild.data;	
 	// Le pasamos los input a las celdas
-	celdas.cells[1].innerHTML = inputNombres;
+	celdas.cells[3].innerHTML = inputNombres;
 	celdas.cells[8].innerHTML = btnGuardar;
 	// Le pasamos el nuevo valor al input
 	document.querySelector('.nombres').value = nombres;
@@ -55,6 +72,6 @@ function guardar(){
 	// Se recibe el nuevo valor
 	nombres = document.querySelector('.nombres').value;
 	// Se pasa el nuevo valor a la celda correspondiente
-	celdas.cells[1].innerHTML = nombres;
+	celdas.cells[3].innerHTML = nombres;
 	celdas.cells[8].innerHTML = btnActualizar + btnEliminar;		
 }
--- app/2_app_vistas_proyecto/js/scripts_crear.js
+++ app/2_app_vistas_proyecto/js/scripts_crear.js
@@ -1,7 +1,24 @@
 form = document.getElementById('crear_usuario');
 form.addEventListener('submit', function (event){
+	// Captura de Datos
+	doc_identidad = document.getElementById('doc_identidad').value;
+	correo = document.getElementById('correo').value;		
 	nombres = document.getElementById('nombres').value;		
-	localStorage.setItem('nombres', nombres);
+	apellidos = document.getElementById('apellidos').value;		
+	contrasena_us = document.getElementById('contrasena_us').value;		
+	confirmacion = document.getElementById('confirmacion').value;		
+	perfil = document.getElementById('perfil').value;		
+	estado = document.getElementById('estado').value;		
+	
+	// Pasar Datos a Variables de Sesión (de un js a otro)
+	sessionStorage.setItem('doc_identidad', doc_identidad);
+	sessionStorage.setItem('correo', correo);
+	sessionStorage.setItem('nombres', nombres);
+	sessionStorage.setItem('apellidos', apellidos);
+	sessionStorage.setItem('contrasena_us', contrasena_us);
+	sessionStorage.setItem('confirmacion', confirmacion);
+	sessionStorage.setItem('perfil', perfil);
+	sessionStorage.setItem('estado', estado);
 	window.location = 'consultar_usuarios.html';
 	event.preventDefault();	
 });
