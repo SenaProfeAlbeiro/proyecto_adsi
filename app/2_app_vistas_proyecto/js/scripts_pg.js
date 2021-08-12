84eaee87bdca0a2b7fc700828c4132c80725d2ba
SenaProfeAlbeiro <earamos42@misena.edu.co>
2021-08-12 06:39:05 -0500

    Terminando Vistas Sistema de Información

--- app/2_app_vistas_proyecto/js/scripts_pg.js
+++ app/2_app_vistas_proyecto/js/scripts_pg.js
@@ -1,35 +1,35 @@
 // Declaración e inicialización de variables
 var tabla = document.getElementById("tbl_consultar");
+var i, celdas, nombres;
+
 
 // Crear controles
 var inputNombres = "<input type='text' class='nombres text-center' autofocus/>";
-var btnGuardar = '<a class="guardar btn btn-success btn-sm mx-1"><i class="fas fa-save"></i></a>';
+var btnGuardar = '<a class="guardar btn btn-success btn-sm mx-1" onclick="guardar()"><i class="fas fa-save"></i></a>';
 
 // Actualizar
 const btnEditar = document.querySelectorAll(".actualizar");
 const actualizar = function (event) {
 	// Obtiene el número de fila seleccionada
-	let i = this.parentNode.parentNode.rowIndex;
-	// Prepara la fila con sus las celdas
-	let celdas = tabla.rows[i];
+	i = this.parentNode.parentNode.rowIndex;
+	// Prepara la fila con sus celdas
+	celdas = tabla.rows[i];
 	// Obtiene el valor de la celda según su posición en la fila
-	let nombres = celdas.cells[1].firstChild.data;
-	
-	// Le pasamos el control input a la celda
-	celdas.cells[1].innerHTML = inputNombres;	
+	nombres = celdas.cells[1].firstChild.data;	
+	// Le pasamos los input a las celdas
+	celdas.cells[1].innerHTML = inputNombres;
 	celdas.cells[8].innerHTML = btnGuardar;
-	
-	document.querySelector('input.nombres').value = nombres;
-	// alert(nombres);	
-
-	
-
-
-
-
-	
-	
+	// Le pasamos el nuevo valor al input
+	document.querySelector('.nombres').value = nombres;
 }
 btnEditar.forEach(boton => {
 	boton.addEventListener("click", actualizar);	
 });
+
+// Guardar
+function guardar(){
+	nombres = document.querySelector('.nombres').value;
+	celdas.cells[1].innerHTML = nombres;
+	// alert(nombres)
+	// celdas.cells[1].innerHTML = nombres;
+}
