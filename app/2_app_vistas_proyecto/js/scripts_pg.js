3705245d39b72cd89aa1f904dde79590ca26d72e
SenaProfeAlbeiro <earamos42@misena.edu.co>
2021-08-12 05:44:23 -0500

    Terminando Vistas Sistema de Información

--- app/2_app_vistas_proyecto/js/scripts_pg.js
+++ app/2_app_vistas_proyecto/js/scripts_pg.js
@@ -2,28 +2,34 @@
 var tabla = document.getElementById("tbl_consultar");
 
 // Crear controles
-var inputNombres = "<input type='text' class='nombres'/>";
+var inputNombres = "<input type='text' class='nombres text-center'/>";
+var btnGuardar = '<a class="guardar btn btn-success btn-sm mx-1"><i class="fas fa-save"></i></a>';
 
 // Actualizar
-const btn_actualizar = document.querySelectorAll(".actualizar");
-const actualizar = function (event) {	
+const btnEditar = document.querySelectorAll(".actualizar");
+const actualizar = function (event) {
+	// Obtiene el número de fila seleccionada
 	let i = this.parentNode.parentNode.rowIndex;
+	// Prepara la fila con sus las celdas
 	let celdas = tabla.rows[i];
-
+	// Obtiene el valor de la celda según su posición en la fila
 	let nombres = celdas.cells[1].firstChild.data;
-
+	
+	// Le pasamos el control input a la celda
 	celdas.cells[1].innerHTML = inputNombres;
+	celdas.cells[8].innerHTML = btnGuardar;
 	
 	document.querySelector('input.nombres').value = nombres;
+	// alert(nombres);	
+
 	
-	event.preventDefault();
 
 
 
 
-	// alert("HolaMundo");
+	
 	
 }
-btn_actualizar.forEach(boton => {
+btnEditar.forEach(boton => {
 	boton.addEventListener("click", actualizar);	
 });
