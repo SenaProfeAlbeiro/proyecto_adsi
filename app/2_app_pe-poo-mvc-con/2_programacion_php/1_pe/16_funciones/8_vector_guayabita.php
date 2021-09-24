ba152deda64a4a6738df6aefdb90bcd5b292cde7
SenaProfeAlbeiro <earamos42@misena.edu.co>
2021-09-24 14:54:03 -0500

    Funciones PHP, vista y controlador

--- app/2_app_pe-poo-mvc-con/2_programacion_php/1_pe/16_funciones/8_vector_guayabita.php
+++ app/2_app_pe-poo-mvc-con/2_programacion_php/1_pe/16_funciones/8_vector_guayabita.php
@@ -1,7 +1,6 @@
 <?php
 
-// declaración e inicialización de variables, constantes y arreglos
-	$nom_aplicacion = "8. Guayabita";
+// declaración e inicialización de variables, constantes y arreglos	
 	$cantJug = null;
 	$pozo = $cantJug;
 	$jugador = null;	
@@ -9,10 +8,108 @@
 	$lanza1 = null;	
 	$lanza2 = null;	
 	$apuesta = null;
-	$res = "Digite la cantidad de Jugadores";
+	$instrucciones = "Digite la Cantidad de Jugadores (El Total de Jugadores es Igual al Pozo) / Escriba los nombres de los Jugadores / Lanzamiento Uno (Si obtiene 1 o 6 coloca una moneda, si es diferente, apuesta entre 1 y el acumulado del Pozo) / Lanzamiento Dos (Si obtiene una suma mayor al lanzamiento Uno, gana lo que apostó, de lo contrario coloca la apuesta) ";
 	$jugadores = [];		
 
 // Funciones
+	# 1. Inicia el Juego
+	function iniciar($cantJug, $jugadores, $posJug){
+		echo 'Inicia el Juego';		
+		// if ($jugadores[$cantJug - 1] != null) {
+		// 	# Asigna el nombre al jugador según su posición					
+		// 	$jugador = $jugadores[$posJug];
+		// 	$instrucciones = "Primer lanzamiento de " . $jugador;
+		// 	# Verifica que el lanzamiento uno no sea nulo
+		// 	if ($lanza1 != null) {
+		// 		$instrucciones = $jugador . " debe Apostar";
+		// 		# Si saca 1 o 6 pierde, de lo contrario, hace un 2do lanzamiento
+		// 		if ($lanza1 != 1 && $lanza1 != 6) {
+		// 			# Verifica que la apuesta no esté vacía
+		// 			if ($apuesta != null) {
+		// 				$instrucciones = "Segundo lanzamiento de " . $jugador;
+		// 				# La apuesta debe ser menor o igual al pozo
+		// 				if ($apuesta <= $pozo) {
+		// 					# Verifica que el lanzamiento dos no sea nulo
+		// 					if ($lanza2 != null) {
+		// 						# Verifica la posición del primer jugador
+		// 						if ($posJug != 0) {
+		// 							$jugador = $jugadores[$posJug];								
+		// 						} else {
+		// 							$posJug;
+		// 							$jugador = $jugadores[$posJug];									
+		// 						}
+		// 						# Compara el lanzamiento uno con el dos
+		// 						if ($lanza2 > $lanza1) {
+		// 							$instrucciones = "Has Ganado " . $jugador . "!!!";
+		// 							$pozo = $pozo - $apuesta;
+		// 						} else {
+		// 							$instrucciones = "Has Perdido tu Apuesta " . $jugador . "!!!";
+		// 							$pozo = $pozo + $apuesta;
+		// 						}
+		// 						# Cambia al jugador siguiente y primer lanzamiento
+		// 						$posJug++;
+		// 						if ($cantJug > $posJug) {
+		// 							$jugador = $jugadores[$posJug];
+		// 						} else {
+		// 							$jugador = $jugadores[0];
+		// 						}
+		// 						$instrucciones .= "<br>Primer lanzamiento de " . $jugador;
+		// 					}
+		// 				} else {
+		// 					$instrucciones = "La apuesta debe ser menor o igual al pozo";
+		// 				}
+		// 			}
+		// 		} else {
+		// 			$instrucciones = "Sacaste 1 o 6. Perdiste " . $jugador . ", coloca una moneda. ";
+		// 			$pozo++;
+		// 			$posJug++;
+		// 			if ($cantJug > $posJug) {
+		// 				$jugador = $jugadores[$posJug];								
+		// 			} else {
+		// 				$jugador = $jugadores[0];
+		// 			}
+		// 			$instrucciones .= "<br>Primer lanzamiento de " . $jugador;					
+		// 		}
+		// 	}
+		// }
+		// # Si la cantJug es igual a posJug, reinicia la posición del jugador
+		// if ($cantJug == $posJug) {
+		// 	$posJug = 0;
+		// 	$jugador = $jugadores[$posJug];
+		// }		
+		// # Si el pozo es igual a cero, entonces reinicia el juego
+		// if ($pozo == 0) {				
+		// 	$instrucciones .= "<br>No hay dinero en el pozo, coloquen la apuesta mínima!!!";
+		// 	$pozo = $cantJug;				
+		// }
+	}
+
+	# 2. Obtener la catindad de Jugadores e inicia el arreglo jugadores
+	function iniciar_jugadores($cantJug){
+		for ($i=0; $i < $cantJug; $i++) { 
+			$jugadores[$i] = null;
+		}
+		return $jugadores;
+	}
+
+	# 4. Validar Nulos en los controles de los Jugadores
+	function validar_nulos($jugadores){
+		foreach ($jugadores as $valor) {
+			if ($valor != null) {
+				$res_local = true;
+			} else {
+				$res_local = false;
+				break;
+			}		
+		}
+		return $res_local;
+	}
+
+
+	# 3. Posicionar el jugador
+	function posicionar_jugador($posJug){
+		
+	}
 
 
 // entrada: 
@@ -21,119 +118,45 @@
 		if (!empty($_POST['cantJug'])) {
 			$cantJug = $_POST['cantJug'];
 			$pozo = $cantJug;
-			for ($i=0; $i < $cantJug; $i++) { 
-				$jugadores[$i] = null;
-			}
-		}
-		# Recibe el nombre de cada jugador
-		if (isset($_POST['jugadores'])) {
-			$jugadores = $_POST['jugadores'];
-			$cantJug = $_POST['cantJug'];
-			$pozo = $_POST['pozo'];
-		}		
-		# Recibe el lanzamiento Uno
-		if (!empty($_POST['lanza1'])) {
-			$lanza1 = $_POST['lanza1'];			
-			$pozo = $_POST['pozo'];
-			$posJug = $_POST['posJug'];
-			$jugador = $_POST['jugador'];
-		}
-		# Recibe la apuesta del jugador
-		if (!empty($_POST['apuesta'])) {
-			$apuesta = $_POST['apuesta'];
-			$pozo = $_POST['pozo'];
-			$posJug = $_POST['posJug'];
-			$jugador = $_POST['jugador'];
-		}
-		# Recibe el lanzamiento Dos
-		if (!empty($_POST['lanza2'])) {
-			$apuesta = $_POST['apuesta'];
-			$lanza2 = $_POST['lanza2'];
-			$pozo = $_POST['pozo'];
-			$posJug = $_POST['posJug'];
-			$jugador = $_POST['jugador'];
-		}			
-		# Verificar datos
-		// echo "Cantidad Jugadores: " . $cantJug;		
-		// echo "<br>Jugadores: ";
-		// print_r($jugadores);		
-		// echo "<br>Posción jugador: " . $posJug;
-		// echo "<br>Nombre jugador: " . $jugador;
-		// echo "<br>Primer Lanzamiento:  " . $lanza1;
-		// echo "<br>Apuesta: " . $apuesta;
-		// echo "<br>Segundo Lanzamiento: " . $lanza2;
+			$jugadores = iniciar_jugadores($cantJug);			
+			$instrucciones = "Digite el nombre de cada uno de los Jugadores";
+			// echo validar_nulos($_POST['jugadores']);
+			if (isset($_POST['jugadores'])) {
+				if (validar_nulos($_POST['jugadores'])) {
+					# Recibe el nombre de cada jugador					
+					$jugadores = $_POST['jugadores'];
+					$posJug = $_POST['posJug'];
+					iniciar($cantJug, $jugadores, $posJug);
+					
 
-// Proceso
-		# Valida que hayan jugadores
-		$res = "Digite el nombre de los Jugadores";
-		if ($jugadores[$cantJug - 1] != null) {
-			# Asigna el nombre al jugador según su posición					
-			$jugador = $jugadores[$posJug];
-			$res = "Primer lanzamiento de " . $jugador;
-			# Verifica que el lanzamiento uno no sea nulo
-			if ($lanza1 != null) {
-				$res = $jugador . " debe Apostar";
-				# Si saca 1 o 6 pierde, de lo contrario, hace un 2do lanzamiento
-				if ($lanza1 != 1 && $lanza1 != 6) {
-					# Verifica que la apuesta no esté vacía
-					if ($apuesta != null) {
-						$res = "Segundo lanzamiento de " . $jugador;
-						# La apuesta debe ser menor o igual al pozo
-						if ($apuesta <= $pozo) {
-							# Verifica que el lanzamiento dos no sea nulo
-							if ($lanza2 != null) {
-								# Verifica la posición del primer jugador
-								if ($posJug != 0) {
-									$jugador = $jugadores[$posJug];								
-								} else {
-									$posJug;
-									$jugador = $jugadores[$posJug];									
-								}
-								# Compara el lanzamiento uno con el dos
-								if ($lanza2 > $lanza1) {
-									$res = "Has Ganado " . $jugador . "!!!";
-									$pozo = $pozo - $apuesta;
-								} else {
-									$res = "Has Perdido tu Apuesta " . $jugador . "!!!";
-									$pozo = $pozo + $apuesta;
-								}
-								# Cambia al jugador siguiente y primer lanzamiento
-								$posJug++;
-								if ($cantJug > $posJug) {
-									$jugador = $jugadores[$posJug];
-								} else {
-									$jugador = $jugadores[0];
-								}
-								$res .= "<br>Primer lanzamiento de " . $jugador;
-							}
-						} else {
-							$res = "La apuesta debe ser menor o igual al pozo";
-						}
+					$instrucciones = 'Péreme Tantico';
+					# Recibe el lanzamiento Uno
+					if (!empty($_POST['lanza1'])) {
+						$lanza1 = $_POST['lanza1'];			
+						$pozo = $_POST['pozo'];
+						$posJug = $_POST['posJug'];
+						$jugador = $_POST['jugador'];
 					}
-				} else {
-					$res = "Sacaste 1 o 6. Perdiste " . $jugador . ", coloca una moneda. ";
-					$pozo++;
-					$posJug++;
-					if ($cantJug > $posJug) {
-						$jugador = $jugadores[$posJug];								
-					} else {
-						$jugador = $jugadores[0];
+					# Recibe la apuesta del jugador
+					if (!empty($_POST['apuesta'])) {
+						$apuesta = $_POST['apuesta'];
+						$pozo = $_POST['pozo'];
+						$posJug = $_POST['posJug'];
+						$jugador = $_POST['jugador'];
 					}
-					$res .= "<br>Primer lanzamiento de " . $jugador;					
+					# Recibe el lanzamiento Dos
+					if (!empty($_POST['lanza2'])) {
+						$apuesta = $_POST['apuesta'];
+						$lanza2 = $_POST['lanza2'];
+						$pozo = $_POST['pozo'];
+						$posJug = $_POST['posJug'];
+						$jugador = $_POST['jugador'];
+					}
 				}
 			}
+		} else {
+			$instrucciones = "Digite la cantidad de Jugadores";
 		}
-		# Si la cantJug es igual a posJug, reinicia la posición del jugador
-		if ($cantJug == $posJug) {
-			$posJug = 0;
-			$jugador = $jugadores[$posJug];
-		}		
-		# Si el pozo es igual a cero, entonces reinicia el juego
-		if ($pozo == 0) {				
-			$res .= "<br>No hay dinero en el pozo, coloquen la apuesta mínima!!!";
-			$pozo = $cantJug;				
-		}
-		
 	}	
 
 // salida
@@ -143,12 +166,12 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?php echo $nom_aplicacion ?></title>
+	<title>8. Guayabita</title>
 </head>
 <body>
 
 	<!-- Nombre de la Aplicación -->
-	<h1><?php echo $nom_aplicacion ?></h1>
+	<h1>8. Guayabita</h1>
 	<p><a href="index.php">Volver</a></p>
 	<hr>
 
@@ -183,7 +206,11 @@
 					  <input type="text" name="jugadores[]" value=' . $jugadores[$i] . '><br>';
 			}
 		?>
-		<br>		
+		<br>
+		<div>
+			<!-- <label>Posición </label> -->
+			<input type="text" name="posJug" value="<?php echo $posJug ?>">
+		</div>		
 		<div>
 			<input type="submit" value="Enviar">
 		</div>
@@ -316,7 +343,7 @@
 		<input type="text" value="<?php echo $jugador ?>" disabled> -->
 		<label>Pozo </label>
 		<input type="text" value="<?php echo $pozo ?>" disabled>
-		<h2 align="center"><?php echo $res ?></h2>		
+		<h2 align="center"><?php echo $instrucciones ?></h2>		
 		<label>Apuesta </label>
 		<input type="text" value="<?php echo $apuesta ?>" disabled>
 		<br><br>
