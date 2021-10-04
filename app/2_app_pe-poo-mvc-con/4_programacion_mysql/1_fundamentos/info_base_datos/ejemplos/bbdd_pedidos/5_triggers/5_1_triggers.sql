fbe53267af5b68a2b1991f76b218dfa975ce73c5
SenaProfeAlbeiro <earamos42@misena.edu.co>
2021-10-04 11:38:49 -0500

    Base de Datos Triggers

--- app/2_app_pe-poo-mvc-con/4_programacion_mysql/1_fundamentos/info_base_datos/ejemplos/bbdd_pedidos/5_triggers/5_1_triggers.sql
+++ app/2_app_pe-poo-mvc-con/4_programacion_mysql/1_fundamentos/info_base_datos/ejemplos/bbdd_pedidos/5_triggers/5_1_triggers.sql
@@ -14,7 +14,7 @@
 
 
 -- -------------------------------------------------------------------------------------
--- INSERTAR UN REGISTRO A PRODUCTOS Y UTILIZAR UN TRIGGER PARA ALMACENAR EN TABLA REG  
+-- INSERTAR REGISTRO A PRODUCTOS Y UTILIZAR UN TRIGGER PARA ALMACENAR EN TABLA REG_PROD  
 -- -------------------------------------------------------------------------------------
 ## Crear una Tabla para almacenar el registro 'reg_productos'
 -- -------------------------------------------------------------------------------------
@@ -30,7 +30,8 @@ CREATE TABLE reg_productos (
 CREATE TRIGGER productos_ai AFTER INSERT ON productos
 FOR EACH ROW INSERT INTO reg_productos (
 	codigo_articulo, nombre_articulo, precio, insertado
-	) VALUES (NEW.codigo_articulo, NEW.nombre_articulo, NEW.precio, NOW()
+) VALUES (
+	NEW.codigo_articulo, NEW.nombre_articulo, NEW.precio, NOW()
 );
 -- -------------------------------------------------------------------------------------
 ## INSERT: Insertar Registro a la Tabla Productos y comprobar
@@ -38,12 +39,10 @@ FOR EACH ROW INSERT INTO reg_productos (
 INSERT INTO productos VALUES
 (NULL, 'CONFECCIÓN','PANTALÓN',50,'2020-07-10',0,'BOGOTÁ',NULL);
 -- -------------------------------------------------------------------------------------
-##  Eliminar un Trigger
--- -------------------------------------------------------------------------------------
-DROP TRIGGER IF EXISTS productos_ai;
--- -------------------------------------------------------------------------------------
 
 
+-- -------------------------------------------------------------------------------------
+-- ACTUALIZAR REGISTRO DE PRODUCTOS Y UTILIZAR TRIGGER PARA ALMACENAR EN TABLA PROD_ACT  
 -- -------------------------------------------------------------------------------------
 ## Crear una tabla para almacenar registro 'productos_actualizados'
 -- -------------------------------------------------------------------------------------
@@ -68,11 +67,90 @@ CREATE TABLE productos_actualizados (
 -- -------------------------------------------------------------------------------------
 ## Crear un Trigger: (BEFORE)
 -- -------------------------------------------------------------------------------------
-
+CREATE TRIGGER actualiza_productos_bu BEFORE UPDATE ON productos
+FOR EACH ROW INSERT INTO productos_actualizados (
+	anterior_codigo_articulo, anterior_seccion, anterior_nombre_articulo, anterior_precio,
+	anterior_fecha, anterior_importado, anterior_pais_origen, nuevo_codigo_articulo,
+	nuevo_seccion, nuevo_nombre_articulo, nuevo_precio, nuevo_fecha, nuevo_importado,
+	nuevo_pais_origen, usuario, f_modif
+) VALUES (
+	OLD.codigo_articulo, OLD.seccion, OLD.nombre_articulo, OLD.precio, OLD.fecha,
+	OLD.importado, OLD.pais_origen,
+	NEW.codigo_articulo, NEW.seccion, NEW.nombre_articulo, NEW.precio, NEW.fecha,
+	NEW.importado, NEW.pais_origen, 
+	CURRENT_USER(), NOW()
+);
+-- -------------------------------------------------------------------------------------
+## UPDATE: Actualizar el precio (30.20 a 50.20) de serrucho y comprobar
+-- -------------------------------------------------------------------------------------
+UPDATE productos SET 
+precio = precio + 20 
+WHERE codigo_articulo = 6;
+-- -------------------------------------------------------------------------------------
 
 
 -- -------------------------------------------------------------------------------------
-## UPDATE: 
+-- ELIMINAR UN REGISTRO DE PRODUCTOS Y UTILIZAR TRIGGER PARA ALMACENAR EN TABLA PROD_ACT  
+-- -------------------------------------------------------------------------------------
+## Crear una tabla para almacenar registro 'productos_eliminados'
+-- -------------------------------------------------------------------------------------
+CREATE TABLE productos_eliminados (
+	 codigo_articulo INT(11),
+	 seccion VARCHAR(50),
+	 nombre_articulo VARCHAR(100),
+	 precio DECIMAL(10,2),
+	 fecha DATE,  
+	 importado TINYINT(1),
+	 pais_origen VARCHAR(50),
+	 usuario VARCHAR(15),
+	 f_modif DATE
+);
+-- -------------------------------------------------------------------------------------
+## Crear un Trigger: (BEFORE)
+-- -------------------------------------------------------------------------------------
+CREATE TRIGGER elimina_productos_ad AFTER DELETE ON productos
+FOR EACH ROW INSERT INTO productos_eliminados (
+	codigo_articulo, seccion, nombre_articulo, precio, fecha, importado, pais_origen
+) VALUES (
+	OLD.codigo_articulo, OLD.seccion, OLD.nombre_articulo, OLD.precio, OLD.fecha,
+	OLD.importado, OLD.pais_origen
+);
+-- -------------------------------------------------------------------------------------
+## DELETE: Eliminar un Registro de la Tabla productos y comprobar
+-- -------------------------------------------------------------------------------------
+DELETE FROM productos WHERE codigo_articulo = 40;
 -- -------------------------------------------------------------------------------------
 
+
+-- -------------------------------------------------------------------------------------
+##  ELIMINAR UN TRIGER
+-- -------------------------------------------------------------------------------------
+DROP TRIGGER productos_ai;
+-- -------------------------------------------------------------------------------------
+
+
+-- -------------------------------------------------------------------------------------
+-- MODIFICAR UN TRIGGER
+-- -------------------------------------------------------------------------------------
+## Agregar dos campos más a la tabla elimina_productos
+-- -------------------------------------------------------------------------------------
+ALTER TABLE productos_eliminados ADD COLUMN (
+	usuario VARCHAR(50), f_modif DATETIME
+); 
+-- -------------------------------------------------------------------------------------
+## Modificar el Trigger elimina_productos_ad de acuerdo a los campos agregados
+-- -------------------------------------------------------------------------------------
+DROP TRIGGER IF EXISTS elimina_productos_ad;
+CREATE TRIGGER elimina_productos_ad AFTER DELETE ON productos
+FOR EACH ROW INSERT INTO productos_eliminados (
+	codigo_articulo, seccion, nombre_articulo, precio, fecha, importado, pais_origen,
+    usuario, f_modif
+) VALUES (
+	OLD.codigo_articulo, OLD.seccion, OLD.nombre_articulo, OLD.precio, OLD.fecha,
+	OLD.importado, OLD.pais_origen, CURRENT_USER(), NOW()
+);
+-- -------------------------------------------------------------------------------------
+## DELETE: Eliminar un Registro de la Tabla productos y comprobar
+-- -------------------------------------------------------------------------------------
+DELETE FROM productos WHERE codigo_articulo = 39;
 -- -------------------------------------------------------------------------------------
